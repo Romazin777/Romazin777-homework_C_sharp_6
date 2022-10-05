43baa9961e47d8f12ad9eae2e437b48cf477353f
Denis Romazin <romazin777@gmail.com>
2022-10-05 19:15:36 +0500

    Add quest 41

---
+++ quest_41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите количество чисел для проверки: ");
+int M = int.Parse(Console.ReadLine()!);
+int[] arr = ArraySize(M);
+Console.WriteLine($"Из чисел: {String.Join(", ", arr)}, положительных будет {PositivNumb(arr)}");
+
+int[] ArraySize(int size)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(-100, 100);
+    }
+    return res;
+}
+int PositivNumb(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) res++;
+    }
+    return res;
+}
